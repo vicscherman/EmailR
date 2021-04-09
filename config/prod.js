94d79222f0dacc96ee33ac5cbf98859ad3c9e499
vicscherman <vicscherman@gmail.com>
2021-04-09 10:46:19 -0400

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+const { googleClientSecret } = require("./dev");
+
+//prod.js --production keys here
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID ,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI ,
+    cookieKey: process.env.COOKIE_KEY
+}
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+//make sure the model is imported before the passport service is used.....
+require("./models/User");
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ bye: 'bud'});
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
+
+const app = express();
+//wiring up middleware 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
