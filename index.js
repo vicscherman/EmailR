e512bfa07772da551ce9979cb22ce0dfb3c0db48
vicscherman <vicscherman@gmail.com>
2021-04-09 11:20:28 -0400

    restored previous state

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 //wiring up middleware 
