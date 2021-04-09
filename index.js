e2d5f5168ad8be74b3fb22535e612cbb2352ee4a
vicscherman <vicscherman@gmail.com>
2021-04-09 11:19:22 -0400

    testing one thing

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 //wiring up middleware 
